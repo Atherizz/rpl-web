2c4e71081580f070c7009a8e50a204d3812b0a19
Atherizz <saveroathallah86@gmail.com>
2024-12-03 19:37:51 +0700

    fix cookie, notif login

--- app/models/Register_model.php
+++ app/models/Register_model.php
@@ -1,6 +1,7 @@
 <?php 
 class Register_model {
     private $db;
+    public $error;
 
     public function __construct() {
         $this->db = new Database();
@@ -15,6 +16,25 @@ class Register_model {
         $this->db->query("SELECT * FROM user WHERE username = :username");
         $this->db->bind('username', $data['username']);
         $user = $this->db->single();
+
+        if ($user) {
+            $this->error = "username telah digunakan!";
+            return false;
+        }
+
+        if ($data['password'] !== $data['passwordVerify']) {
+            $this->error = "verifikasi password tidak sesuai!";
+            return false;
+        }
+      
+        $password = hash("sha256", $data['password']);
+
+        $this->db->query("INSERT INTO user (username, password) VALUES (:username, :password)");
+        $this->db->bind('username', $data['username']);
+        $this->db->bind('password', $password);
+        $this->db->execute();
+    
+        return $this->db->rowCount();
     }
 }
 ?>
